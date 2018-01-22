dff76b567bccd07b22adedc5e664e4b020dd585e
Marcello Galvan <emyb@bellsouth.net>
2018-01-22 16:19:07 -0500

    initialize GL context and make canvas black

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,21 @@
+// Primary execution
 console.log('hello world!')
+main()
+
+// Functions
+function main() {
+  const canvas = document.querySelector('#canvas')
+  // initialize GL context
+  const gl = canvas.getContext('webgl')
+
+  // Only continue if WebGL is available and working
+  if (!gl) {
+    alert('Unable to initialize WebGL. Your browser or machine may not support it')
+    return
+  }
+
+  // Set clear color to opaque black
+  gl.clearColor(0.0, 0.0, 0.0, 1.0)
+  // Clear the color buffer with specified clear clearColor
+  gl.clear(gl.COLOR_BUFFER_BIT)
+}
